e199400024b278fad03c1d98ae9c10af7d6f02c3
kgp_tmp <fgfg235@gmail.com>
2024-08-07 04:32:23 +0900

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import App from './App'
 import './index.css'
 import { GlobalStyle } from "./components/common/styles/global.jsx";
 import {Provider} from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <HashRouter>
         <GlobalStyle />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
   </Provider>
 )

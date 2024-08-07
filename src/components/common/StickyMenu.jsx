de635d4d7dcf1f945a55813608e641f17f0a6a18
kgp_tmp <fgfg235@gmail.com>
2024-08-07 10:05:16 +0900

    assets commit

--- src/components/common/StickyMenu.jsx
+++ src/components/common/StickyMenu.jsx
@@ -52,7 +52,73 @@ class StickyMenu extends React.Component {
                             </Col>
                             <Col xl="9" md="10">
                                 <div className="sticky-menu-box d-flex justify-content-center">
-                                    frontend/src/components/header/Header.jsx
+                                <ul className="nav menu-nav">
+                                            <li className="nav-item dropdown active mx-3">
+                                                <Link className="nav-link dropdown-toggle" to="/" data-toggle="dropdown">소개 <i className="bi bi-chevron-down"></i></Link>
+                                                <ul className="dropdown list-unstyled">
+                                                    <li className="nav-item active"><Link className="nav-link" to="/board/About">인사말</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/About">추진위원회</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/About">비전</Link></li>
+                                                </ul>
+                                            </li>
+                                            <li className="nav-item dropdown mx-3">
+                                                <Link className="nav-link dropdown-toggle" to="/" data-toggle="dropdown">NGEF 2024 <i className="bi bi-chevron-down"></i></Link>
+                                                <ul className="dropdown list-unstyled">
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/About">개요</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/Shedule">전체 일정표</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/About">기조 연설</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/Session">세부세션</Link></li>
+                                                    {/* <li className="nav-item"><Link className="nav-link" to="/faq">Faq</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/error">404</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/coming-soon">Coming Soon</Link></li> */}
+                                                </ul>
+                                            </li>
+                                            <li className="nav-item dropdown mx-3">
+                                                <Link className="nav-link dropdown-toggle" to="/" data-toggle="dropdown">부대행사 <i className="bi bi-chevron-down"></i></Link>
+                                                <ul className="dropdown list-unstyled">
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/About">리더스 포럼</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/">연계프로그램</Link></li>
+                                                </ul>
+                                            </li>
+                                            <li className="nav-item dropdown mx-3">
+                                                <Link className="nav-link dropdown-toggle" to="/" data-toggle="dropdown">참가신청 <i className="bi bi-chevron-down"></i></Link>
+                                                <ul className="dropdown list-unstyled">
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/RegistForum">참가신청</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/project-two">참가신청확인</Link></li>
+                                                    {/* <li className="nav-item"><Link className="nav-link" to="/project-details">Project Details</Link></li> */}
+                                                </ul>
+                                            </li>
+                                            <li className="nav-item dropdown mx-3">
+                                                <Link className="nav-link dropdown-toggle" to="/" data-toggle="dropdown">전시 <i className="bi bi-chevron-down"></i></Link>
+                                                <ul className="dropdown list-unstyled">
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/Schedule">전체배치도</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/Schedule">참가기업</Link></li>
+                                                    {/* <li className="nav-item"><Link className="nav-link" to="/blog-classic">Blog Classic</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/blog-grid">Blog Grid</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/blog-details">Blog Details</Link></li> */}
+                                                </ul>
+                                            </li>
+                                            <li className="nav-item dropdown mx-3">
+                                                <Link className="nav-link dropdown-toggle" to="/" data-toggle="dropdown">아카이브_hidden 예정<i className="bi bi-chevron-down"></i></Link>
+                                                <ul className="dropdown list-unstyled">
+                                                    <li className="nav-item"><Link className="nav-link" to="/board/Schedule">2024</Link></li>
+                                                </ul>
+                                            </li>
+                                            <li className="nav-item">
+                                                <Link className="nav-link" to="/board/Schedule">LIVE</Link>
+                                            </li>
+                                            <li className="nav-item dropdown mx-3">
+                                                <Link className="nav-link dropdown-toggle" to="/" data-toggle="dropdown">정보센터 <i className="bi bi-chevron-down"></i></Link>
+                                                <ul className="dropdown list-unstyled">
+                                                    <li className="nav-item"><Link className="nav-link" to="/project-one">공지사항</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/project-two">E-브로셔</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/project-one">오시는 길</Link></li>
+                                                    <li className="nav-item"><Link className="nav-link" to="/project-one">문의</Link></li>
+                                                    {/* <li className="nav-item"><Link className="nav-link" to="/project-details">Project Details</Link></li> */}
+                                                </ul>
+                                            </li>
+                                            {/* <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li> */}
+                                        </ul>
                                 </div>
                             </Col>
                         </Row>
